fc1b34f360eebe2d53c2dbc2d57f18954666b0f1
jxybyte <ramosjayln@gmail.com>
2026-06-23 13:02:53 -0400

    fixed use categoryservice for getproductsbycategoryid in catergoriescontroller

--- capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -51,7 +51,7 @@ public class CategoriesController {
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return productService.getProductsByCategoryId(categoryId);
+        return categoryService.getProductsByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
