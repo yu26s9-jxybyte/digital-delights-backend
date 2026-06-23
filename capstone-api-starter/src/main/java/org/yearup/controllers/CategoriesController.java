bcffa7ae04ccf41464f98779df0abadeeab6b36b
jxybyte <ramosjayln@gmail.com>
2026-06-23 08:35:55 -0400

    implement GET /categories/{id} endpoint and products endpoint

--- capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -36,10 +36,11 @@ public class CategoriesController {
         return categoryService.getAllCategories();
     }
     // add the appropriate annotation for a get action
+    @GetMapping("/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryService.getCategoryById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -48,7 +49,7 @@ public class CategoriesController {
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return productService.getProductsByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
