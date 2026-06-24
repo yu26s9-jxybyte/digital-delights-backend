8d9c2b5a712b8adb916a00b1b30f9e0351aed4a1
jxybyte <ramosjayln@gmail.com>
2026-06-24 15:53:33 -0400

    Phase 3: added put method to update exisiting product in cart

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,11 +1,9 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -48,12 +46,35 @@ public class ShoppingCartController
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
     // return the updated cart with status 201 Created
+    @PostMapping("/products/{productId}")
+    public ResponseEntity<ShoppingCart> addProductToCart(@PathVariable int productId, Principal principal)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
 
+        ShoppingCart updatedCart = shoppingCartService.addProduct(userId, productId);
+
+        return ResponseEntity.status(201).body(updatedCart);
+    }
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated; return the cart (200 OK)
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<ShoppingCart> updateProduct(
+            @PathVariable int productId,
+            @RequestBody ShoppingCart item,
+            Principal principal)
+        {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
 
+        ShoppingCart updatedCart = shoppingCartService.updateProduct(userId, productId, item.getQuantity());
+        return ResponseEntity.ok(200).body(updatedCart);
+
+        }
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart  - return the (now empty) cart so the front end can refresh it (200 OK)
