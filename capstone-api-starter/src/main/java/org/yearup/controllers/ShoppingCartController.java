a829bb6d773a7c61f02f1514c3d5dfca5312c66a
jxybyte <ramosjayln@gmail.com>
2026-06-24 15:36:19 -0400

    Phase 3 shopping cart: added RestController, autowired and getmapping

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,11 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -9,15 +15,24 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin
+@PreAuthorize("isAuthenticaed()") //only logged in users can access the cart
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
     private ShoppingCartService shoppingCartService;
     private UserService userService;
 
-
-
+    @Autowired
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService)
+    {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
     public ShoppingCart getCart(Principal principal)
     {
         // get the currently logged in username
@@ -27,7 +42,7 @@ public class ShoppingCartController
         int userId = user.getId();
 
         // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        return shoppingCartService.getCart(userId);
     }
 
     // add a POST method to add a product to the cart - the url should be
